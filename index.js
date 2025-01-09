7a57a6c36ce29c0a8a23f074c60018e88c9e203d
Sourabh <sppathak1428@gmail.com>
2025-01-09 23:55:01 +0530

    da

--- index.js
+++ index.js
@@ -278,7 +278,7 @@ app.get('/khushi-send', async (req, res) => {
       "khushibanchhor21@gmail.com",
       "Application for SDE-1",
       "bunch",
-      { "bunchID": "test" }
+      { "bunchID": "linkedin_test_3" }
     );
 
     if (emailData) {
