04e10c8921afa325ca9361e58ac797ad7655405c
Sourabh <sppathak1428@gmail.com>
2025-02-18 00:05:26 +0530

    dsa

--- index.js
+++ index.js
@@ -278,7 +278,7 @@ app.get('/khushi-send', async (req, res) => {
       "khushibanchhor21@gmail.com",
       "Application for SDE-1",
       "bunch",
-      { "bunchID": "linkedin_test_6" }
+      { "bunchID": "linkedin_test_7" }
     );
 
     if (emailData) {
